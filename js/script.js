533aeca46bd5c4445e1011e75f40f1aa56356932
WangTianQiY <13651265996@163.com>
2024-06-27 20:35:25 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,3 +27,19 @@ function login(){
     }
 
 }
+function sign() {
+    let emailInput = document.getElementById("signUpEmail");
+    let passwordInput = document.getElementById("signUpPassword");
+    let usernameInput = document.getElementById("signUpName");
+
+    let email = emailInput.value;
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+
+    if (username === "" || password === "" ||email === "") {
+        alert("邮箱或用户名或密码不能为空");
+    } else {
+        alert("注册且登录成功");
+        window.open("book.html");
+    }
+}
